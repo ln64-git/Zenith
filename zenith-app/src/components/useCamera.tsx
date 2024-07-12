df046f3000896f38ef7b5e077331f26ad9a3ce03
ln64 <ln64.ohio@gmail.com>
2024-07-11 20:49:53 -0400

    camera loaded

---
+++ zenith-app/src/components/useCamera.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useEffect, useRef } from "react";
+
+const useCamera = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const getCameraStream = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Error accessing camera: ", err);
+      }
+    };
+
+    getCameraStream();
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = (videoRef.current.srcObject as MediaStream).getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  return videoRef;
+};
+
+export default useCamera;
